b987dda5131fcf6fc58e045ab9797cb74be966ec
Administrator <info@ericssonbajnoksag.hu>
2019-10-30 22:07:30 +0100

    alfateszt4

--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@
 #include <iostream>
 #include <sstream>
 
-enum class Direction : char {
+enum class Direction : char
+{
     LEFT = 'l',
     RIGHT = 'r'
 };
 
-struct Data {
-    unsigned int currRouter;
+struct Data
+{
+    unsigned int currRouter; ///
     unsigned int currStoreId;
     unsigned int dataIndex;
     unsigned int messageId;
@@ -19,12 +21,14 @@ struct Data {
     Direction dir;
 };
 
-struct MessagePiece {
+struct MessagePiece
+{
     int index;
     std::string message;
 };
 
-struct Reader {    
+struct Reader
+{
     std::array<unsigned int, 3> data;
     std::string previous;
     std::array<std::array<bool, 10>, 14> routerBits;
@@ -34,49 +38,59 @@ struct Reader {
 };
 
 
-void readData(Reader& to) {
+void readData(Reader& to) // void barmi(int a)
+{
     std::string line;
-    std::cerr<<"\n";
-  ///  std::cerr<<"A vonal adatai: "<<line<<"\n";
     to.dataArray.clear();
     to.receivedPieces.clear();
-    
-    while (std::getline(std::cin, line)) {
-        std::cerr<<"A vonal adatai: "<<line
-        <<std::endl;
+
+    while (std::getline(std::cin, line))
+    {
         if (!line.rfind(".", 0))
             return;
-        
+
         if (!line.rfind("WRONG", 0) ||
-            !line.rfind("SCORE", 0) ||
-            !line.rfind("TICK", 0))
+                !line.rfind("SCORE", 0) ||
+                !line.rfind("TICK", 0))
         {
             to.hasEnd = true;
             to.previous = std::move(line);
-        } else if (!line.rfind("REQUEST", 0)) {
+        }
+        else if (!line.rfind("REQUEST", 0))
+        {
             std::stringstream(std::move(line).substr(8)) >> to.data[0] >> to.data[1] >> to.data[2];
-        } else if (!line.rfind("PREVIOUS", 0)) {
+        }
+        else if (!line.rfind("PREVIOUS", 0))
+        {
             to.previous = std::move(line).substr(9);
-        } else if (!line.rfind("ROUTER", 0)) {
+        }
+        else if (!line.rfind("ROUTER", 0))
+        {
             unsigned int routerIndex;
             std::istringstream(line.substr(7)) >> routerIndex >> line;
             auto it = line.begin();
             for (bool& routers : to.routerBits[routerIndex])
                 routers = *it++ == '1';
-        } else if (!line.rfind("DATA", 0)) {
+        }
+        else if (!line.rfind("DATA", 0))
+        {
             Data& curr = to.dataArray.emplace_back();
             std::istringstream(std::move(line).substr(5))
-                >> curr.currRouter
-                >> curr.currStoreId
-                >> curr.dataIndex
-                >> curr.messageId
-                >> curr.fromRouter
-                >> curr.toRouter
-                >> reinterpret_cast<char&>(curr.dir);
-        } else if (!line.rfind("MESSAGE")) {
+                    >> curr.currRouter
+                    >> curr.currStoreId
+                    >> curr.dataIndex
+                    >> curr.messageId
+                    >> curr.fromRouter
+                    >> curr.toRouter
+                    >> reinterpret_cast<char&>(curr.dir);
+        }
+        else if (!line.rfind("MESSAGE"))
+        {
             MessagePiece& msg = to.receivedPieces.emplace_back();
             std::istringstream(std::move(line).substr(8)) >> msg.index >> msg.message;
-        } else {
+        }
+        else
+        {
             std::cerr << "READER ERROR HAPPENED: unrecognized command line: " << line << std::endl;
             to.hasEnd = true;
             return;
@@ -86,27 +100,33 @@ void readData(Reader& to) {
     to.hasEnd = true;
 }
 
-int main() {
+int main()
+{
     char teamToken[] = "tqEzVLvbq6wz_uWr6HS1";
-    int seed = 0;
-    std::cerr<<"Menj az a***ba! ";
-    std::cout << "START " << teamToken 
-        << " " << seed 
-        << std::endl;
+    // int seed = 0;
+
+    std::cout << "START " << teamToken
+              // << " " << seed
+              << std::endl;
 
     Reader reader = {};
-
-    while(true) {
+    std::string command = "CREATE 1 0";
+    while(true)
+    {
         readData(reader);
-            
-        std::cout<<"REQUEST "<<reader.data[0] << " " << reader.data[1] << " " << reader.data[2] <<std::endl;
-        
+
         if (reader.hasEnd)
             break;
-       
+
+        // TODO logika jobb mint a semmitteves
+        
+
+        // Ha szeretnetek debug uzenetet kuldeni, akkor megtehetitek.
+        // Vigyazzatok, mert maximalisan csak 1024 * 1024 bajtot kaptok vissza
+        std::cerr << "Send " << command << std::endl;
+
         // standard out-ra meg mehet ki a megoldas! Mas ne irodjon ide ki ;)
-        std::cout << reader.data[0] << " " << reader.data[1] << " " << reader.data[2] << " " << "CREATE 69 69"<< std::endl <<".";
-        std::cerr << reader.data[0] << " " << reader.data[1] << " " << reader.data[2] << " " << "CREATE 69 69"<< std::endl <<".";
+        std::cout << reader.data[0] << " " << reader.data[1] << " " << reader.data[2] << " " << command << std::endl;
     }
     std::cerr << "END (latest message): " << reader.previous << std::endl;
 }
