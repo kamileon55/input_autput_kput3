6516a14c59413af60d5eb83c3d7571c23112d35e
Administrator <info@ericssonbajnoksag.hu>
2019-11-11 23:36:24 +0100

    Masodik kör I.5

--- main.cpp
+++ main.cpp
@@ -224,6 +224,7 @@ int main()
 
 
         std::cout << reader.data[0] << " " << reader.data[1] << " " << reader.data[2] << " " << command << std::endl;
+        std::cerr << reader.data[0] << " " << reader.data[1] << " " << reader.data[2] << " " << command << std::endl;
         if(reader.receivedPieces.size()>100)
         {
             //  break;
