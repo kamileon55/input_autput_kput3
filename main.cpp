790c4500c92e7573cd38deaec2535fa788b1a386
Administrator <info@ericssonbajnoksag.hu>
2019-11-13 22:06:31 +0100

    Debug7

--- main.cpp
+++ main.cpp
@@ -384,6 +384,7 @@ int main()
 
 
         std::cout << reader.data[0] << " " << reader.data[1] << " " << reader.data[2] << " " << command << std::endl;
+		std::cerr<<"Command: "<<command<<"\n";
     }
 
     for(int i=0; i<azenyem.size(); i++)
