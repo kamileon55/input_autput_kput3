ee973ed43a9a619f0cc402652929160fd71c1e0a
Administrator <info@ericssonbajnoksag.hu>
2019-11-13 22:32:23 +0100

    Debug12_2

--- main.cpp
+++ main.cpp
@@ -139,17 +139,16 @@ void simulateAction(std::array < std::array<bool, 10>, 14> routerBits, std::vect
 		std::cerr<<"i";
 		//Automatic upwards shift
 		for(int i = 0; i<packets.size(); i++)
-			if (packets[i].currStoreId > 0)
+			if (packets[i].currStoreId > 0 && state[packets[i].currRouter][packets[i].currStoreId - 1] == '-')
 			{
-				if (state[packets[i].currRouter][packets[i].currStoreId - 1] == '-')
-				{
-					state[packets[i].currRouter][packets[i].currStoreId - 1] = '0'+i;
-					state[packets[i].currRouter][packets[i].currStoreId] = '-';
-					packets[i].currStoreId--;
-					std::cerr<<"s";
-					goto togo;
-				}
-				
+				std::cerr<<".";
+				state[packets[i].currRouter][packets[i].currStoreId - 1] = '0'+i;
+				std::cerr<<".";
+				state[packets[i].currRouter][packets[i].currStoreId] = '-';
+				std::cerr<<".";
+				packets[i].currStoreId--;
+				std::cerr<<"s";
+				goto togo;
 			}
 
 		//Automatic movement
